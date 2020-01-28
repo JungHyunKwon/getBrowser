017d01eaaad0018af9953c874a8fe585e82b68e8
JungHyunKwon <kjh_3859@naver.com>
2020-01-28 21:32:37 +0900

    published

--- js/getBrowser.js
+++ js/getBrowser.js
@@ -5,49 +5,49 @@
 (function() {
 	'use strict';
 
-	var _browser = navigator.userAgent.toLowerCase();
+	var browser = navigator.userAgent.toLowerCase();
 
 	//ie7일 때
-	if(_browser.indexOf('msie 7.0') > -1) {
-		_browser = 'ie7';
+	if(browser.indexOf('msie 7.0') > -1) {
+		browser = 'ie7';
 
 	//ie8일 때
-	}else if(_browser.indexOf('msie 8.0') > -1) {
-		_browser = 'ie8';
+	}else if(browser.indexOf('msie 8.0') > -1) {
+		browser = 'ie8';
 
 	//ie9일 때
-	}else if(_browser.indexOf('msie 9.0') > -1) {
-		_browser = 'ie9';
+	}else if(browser.indexOf('msie 9.0') > -1) {
+		browser = 'ie9';
 
 	//ie10일 때
-	}else if(_browser.indexOf('msie 10.0') > -1) {
-		_browser = 'ie10';
+	}else if(browser.indexOf('msie 10.0') > -1) {
+		browser = 'ie10';
 
 	//ie11일 때
-	}else if(_browser.indexOf('trident/7.0') > -1) {
-		_browser = 'ie11';
+	}else if(browser.indexOf('trident/7.0') > -1) {
+		browser = 'ie11';
 
 	//edge일 때
-	}else if(_browser.indexOf('edge') > -1) {
-		_browser = 'edge';
+	}else if(browser.indexOf('edge') > -1) {
+		browser = 'edge';
 
 	//opera일 때
-	}else if(_browser.indexOf('opr') > -1) {
-		_browser = 'opera';
+	}else if(browser.indexOf('opr') > -1) {
+		browser = 'opera';
 
 	//chrome일 때
-	}else if(_browser.indexOf('chrome') > -1) {
-		_browser = 'chrome';
+	}else if(browser.indexOf('chrome') > -1) {
+		browser = 'chrome';
 
 	//firefox일 때
-	}else if(_browser.indexOf('firefox') > -1) {
-		_browser = 'firefox'; 
+	}else if(browser.indexOf('firefox') > -1) {
+		browser = 'firefox'; 
 
 	//safari일 때
-	}else if(_browser.indexOf('safari') > -1) {
-		_browser = 'safari';
+	}else if(browser.indexOf('safari') > -1) {
+		browser = 'safari';
 	}else{
-		_browser = 'unknown';
+		browser = 'unknown';
 	}
 
 	/**
@@ -56,6 +56,6 @@
 	 * @return {string}
 	 */
 	window.getBrowser = function() {
-		return _browser;
+		return browser;
 	};
 })();
