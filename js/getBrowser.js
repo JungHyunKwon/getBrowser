d45a540aa452f7f888e4f83a2b2bffdc7ff9bb90
JungHyunKwon <kjh_3859@naver.com>
2020-01-28 21:35:19 +0900

    published

--- js/getBrowser.js
+++ js/getBrowser.js
@@ -7,43 +7,47 @@
 
 	var browser = navigator.userAgent.toLowerCase();
 
-	//ie7일 때
-	if(browser.indexOf('msie 7.0') > -1) {
+	//IE6일 때
+	if(browser.indexOf('msie 6.0') > -1) {
+		browser = 'ie6';
+
+	//IE7일 때
+	}else if(browser.indexOf('msie 7.0') > -1) {
 		browser = 'ie7';
 
-	//ie8일 때
+	//IE8일 때
 	}else if(browser.indexOf('msie 8.0') > -1) {
 		browser = 'ie8';
 
-	//ie9일 때
+	//IE9일 때
 	}else if(browser.indexOf('msie 9.0') > -1) {
 		browser = 'ie9';
 
-	//ie10일 때
+	//IE10일 때
 	}else if(browser.indexOf('msie 10.0') > -1) {
 		browser = 'ie10';
 
-	//ie11일 때
+	//IE11일 때
 	}else if(browser.indexOf('trident/7.0') > -1) {
 		browser = 'ie11';
 
-	//edge일 때
+	//Edge일 때
 	}else if(browser.indexOf('edge') > -1) {
 		browser = 'edge';
 
-	//opera일 때
+	//Opera일 때
 	}else if(browser.indexOf('opr') > -1) {
 		browser = 'opera';
 
-	//chrome일 때
+	//Chrome일 때
 	}else if(browser.indexOf('chrome') > -1) {
 		browser = 'chrome';
 
-	//firefox일 때
+	//Firefox일 때
 	}else if(browser.indexOf('firefox') > -1) {
 		browser = 'firefox'; 
 
-	//safari일 때
+	//Safari일 때
 	}else if(browser.indexOf('safari') > -1) {
 		browser = 'safari';
 	}else{
